e3f5c95251b6d73926ecceca2b6bc220fe4474a0
Artur Zinnatullin <azinnatu@e1z2r12p3.42.us.org>
2017-10-11 23:39:11 -0700

    fixed img to HTML

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-var formURL = 'https://igpi.ga/spacex/media/';
-$.getJSON(formURL, function(api_data) {
-  console.log(api_data.items);
-
-  // Creating list of articles
-  api_data.items.forEach(function(item, index) {
-
+  var formURL = 'https://igpi.ga/spacex/media/';
+  $.getJSON(formURL, function(api_data) {
     console.log(api_data.items);
+
+
+    var i = 0;
+    api_data.items.forEach(function(key, value) {
+
+      console.log(api_data.items[1].images.standard_resolution.url);
+      output.innerHTML += '<li>' + '<a href=#>' + api_data.items[i].images.standard_resolution.url + '</a>' + '</li>';
+
+
+      output.innerHTML += "<img src=" + api_data.items[i].images.standard_resolution.url + " />"
+      i++;
+
+    });
   });
-});
 });
